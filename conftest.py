c64a09e471931101251f6abcde8ae09b0b70d652
fidail_shafikov <fidail_shafikov@enot.work>
2023-11-02 12:50:47 +0300

    правки

--- conftest.py
+++ conftest.py
@@ -48,47 +48,47 @@ def get_delayed_response():
 
 @pytest.fixture  # 8
 def post_create():
-    response = requests.get(base_url_post_create)
+    response = requests.post(base_url_post_create)
     return response
 
 
 @pytest.fixture  # 9
 def post_register_successful():
-    response = requests.get(base_url_post_register_successful)
+    response = requests.post(base_url_post_register_successful)
     return response
 
 
 @pytest.fixture  # 10
 def post_register_unsuccessful():
-    response = requests.get(base_url_post_register_unsuccessful)
+    response = requests.post(base_url_post_register_unsuccessful)
     return response
 
 
 @pytest.fixture  # 11
 def post_login_successful():
-    response = requests.get(base_url_post_login_successful)
+    response = requests.post(base_url_post_login_successful)
     return response
 
 
 @pytest.fixture  # 12
 def post_login_unsuccessful():
-    response = requests.get(base_url_post_login_unsuccessful)
+    response = requests.post(base_url_post_login_unsuccessful)
     return response
 
 
 @pytest.fixture  # 13
 def put_update():
-    response = requests.get(base_url_put_update)
+    response = requests.put(base_url_put_update)
     return response
 
 
 @pytest.fixture  # 14
 def patch_update():
-    response = requests.get(base_url_patch_update)
+    response = requests.patch(base_url_patch_update)
     return response
 
 
 @pytest.fixture  # 15
 def delete_delete():
-    response = requests.get(base_url_delete_delete)
+    response = requests.delete(base_url_delete_delete)
     return response
