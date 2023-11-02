4b4bd0a1025d8b0d8841ee12e879ab6a1238a882
fidail_shafikov <fidail_shafikov@enot.work>
2023-11-02 15:11:40 +0300

    правки

--- conftest.py
+++ conftest.py
@@ -4,91 +4,91 @@ import requests
 from settings import *
 
 
-@pytest.fixture  # 1
+@pytest.fixture
 def get_list_users():
     response = requests.get(base_url_get_list_users)
     return response
 
 
-@pytest.fixture  # 2
+@pytest.fixture
 def get_single_user():
     response = requests.get(base_url_get_single_user)
     return response
 
 
-@pytest.fixture  # 3
+@pytest.fixture
 def get_single_user_not_found():
     response = requests.get(base_url_get_single_user_not_found)
     return response
 
 
-@pytest.fixture  # 4
+@pytest.fixture
 def get_list_resource():
     response = requests.get(base_url_get_list_resource)
     return response
 
 
-@pytest.fixture  # 5
+@pytest.fixture
 def get_single_resource():
     response = requests.get(base_url_get_single_resource)
     return response
 
 
-@pytest.fixture  # 6
+@pytest.fixture
 def get_single_resource_not_found():
     response = requests.get(base_url_get_single_resource_not_found)
     return response
 
 
-@pytest.fixture  # 7
+@pytest.fixture
 def get_delayed_response():
     response = requests.get(base_url_get_delayed_response)
     return response
 
 
-@pytest.fixture  # 8
-def post_create():
-    response = requests.post(base_url_post_create)
+@pytest.fixture
+def post_create(data):
+    response = requests.post(base_url_post_create, data)
     return response
 
 
-@pytest.fixture  # 9
-def post_register_successful():
-    response = requests.post(base_url_post_register_successful)
+@pytest.fixture
+def post_register_successful(data):
+    response = requests.post(base_url_post_register_successful, data)
     return response
 
 
-@pytest.fixture  # 10
-def post_register_unsuccessful():
-    response = requests.post(base_url_post_register_unsuccessful)
+@pytest.fixture
+def post_register_unsuccessful(data):
+    response = requests.post(base_url_post_register_unsuccessful, data)
     return response
 
 
-@pytest.fixture  # 11
-def post_login_successful():
-    response = requests.post(base_url_post_login_successful)
+@pytest.fixture
+def post_login_successful(data):
+    response = requests.post(base_url_post_login_successful, data)
     return response
 
 
-@pytest.fixture  # 12
-def post_login_unsuccessful():
-    response = requests.post(base_url_post_login_unsuccessful)
+@pytest.fixture
+def post_login_unsuccessful(data):
+    response = requests.post(base_url_post_login_unsuccessful, data)
     return response
 
 
-@pytest.fixture  # 13
-def put_update():
-    response = requests.put(base_url_put_update)
+@pytest.fixture
+def put_update(data):
+    response = requests.put(base_url_put_update, data)
     return response
 
 
-@pytest.fixture  # 14
-def patch_update():
-    response = requests.patch(base_url_patch_update)
+@pytest.fixture
+def patch_update(data):
+    response = requests.patch(base_url_patch_update, data)
     return response
 
 
-@pytest.fixture  # 15
+@pytest.fixture
 def delete_delete():
     response = requests.delete(base_url_delete_delete)
     return response
